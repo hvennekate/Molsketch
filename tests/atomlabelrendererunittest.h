174b51e89b64a430a01eee574e482239348ee7e1
Hendrik Vennekate <Hendrik.Vennekate@posteo.de>
2020-04-13 15:04:56 +0200

    Trying to extract test constants to elimintate system-dependence of font tests
    Note some of these may be inaccurate

--- tests/atomlabelrendererunittest.h
+++ tests/atomlabelrendererunittest.h
@@ -44,6 +44,10 @@ class AtomLabelRendererUnitTest : public CxxTest::TestSuite {
 private:
   AtomLabelRenderer renderer;
 
+  QFontMetricsF ATOM_FONT_METRICS, SCRIPT_FONT_METRICS;
+  qreal TEST_LABEL_WIDTH, TEST3_LABEL_WIDTH, ATOM_FONT_HEIGHT, LETTER_T_WIDTH, LETTER_T_WITH_3_WIDTH, AB_WIDTH, H_WIDTH, H2_WIDTH, TWO_LINE_HEIGHT, THREE_LINE_HEIGHT, SVG_Y_ORIGIN, SVG_Y_SUBSCRIPT, SVG_LETTER_T_WIDTH, SVG_TEST_WIDTH, SVG_TE_WIDTH, SVG_TE3_WIDTH, SVG_X_LEADING_SUB, SVG_X_AFTER_SUB, SVG_AB_WIDTH, SVG_Y_SECOND_LINE, SVG_Y_THIRD_LINE;
+
+
   QString getLabelRenderedAsSvg(const QString &text, const Alignment &alignment) {
     QByteArray ba;
     QBuffer buffer(&ba);
@@ -72,104 +76,171 @@ private:
     auto actualBounds = renderer.computeBoundingRect(text, FONTS, alignment);
     QS_ASSERT_EQUALS(actualBounds, expectedBounds);
   }
+
+  QString svgDimensions(qreal x, qreal y, int fontSize) {
+    return QString("%1, %2, Times, %3").arg(x).arg(y).arg(fontSize);
+  }
 public:
+  AtomLabelRendererUnitTest()
+    : ATOM_FONT_METRICS(ATOM_FONT),
+      SCRIPT_FONT_METRICS(SCRIPT_FONT)
+  {}
+
+  void setUp() override {
+    TEST_LABEL_WIDTH = ATOM_FONT_METRICS.width("test");
+    TEST3_LABEL_WIDTH = TEST_LABEL_WIDTH + SCRIPT_FONT_METRICS.width("3");
+    ATOM_FONT_HEIGHT = ATOM_FONT_METRICS.height();
+    LETTER_T_WIDTH = ATOM_FONT_METRICS.width("t");
+    LETTER_T_WITH_3_WIDTH = -3.4921875;
+    AB_WIDTH = ATOM_FONT_METRICS.width("AB");
+    H_WIDTH = ATOM_FONT_METRICS.width("H");
+    H2_WIDTH = H_WIDTH + SCRIPT_FONT_METRICS.width("2");
+    TWO_LINE_HEIGHT = ATOM_FONT_HEIGHT + ATOM_FONT_METRICS.ascent();
+    THREE_LINE_HEIGHT = TWO_LINE_HEIGHT + ATOM_FONT_METRICS.ascent();
+    SVG_Y_ORIGIN = ATOM_FONT_HEIGHT/2 - ATOM_FONT_METRICS.descent();
+    SVG_Y_SUBSCRIPT = SVG_Y_ORIGIN + ATOM_FONT_METRICS.descent();
+    SVG_LETTER_T_WIDTH = 1.9375*2;
+    SVG_TEST_WIDTH = 17.25;
+    SVG_TE_WIDTH = 8.14063;
+    SVG_TE3_WIDTH = 13.1406;
+    SVG_X_LEADING_SUB = -3.48438;
+    SVG_X_AFTER_SUB = 1.5;
+    SVG_AB_WIDTH = 9.703125*2;
+    SVG_Y_SECOND_LINE = SVG_Y_ORIGIN + ATOM_FONT_METRICS.ascent();
+    SVG_Y_THIRD_LINE = SVG_Y_SECOND_LINE + ATOM_FONT_METRICS.ascent();
+  }
+
   void testSimpleAtomLabel() {
-    assertLabelAndBoundingBox("test", {"-1.9375, 4.5, Times, 10", "test"}, {-1.9375, -8.5, 19.390625, 17});
+    assertLabelAndBoundingBox("test", {svgDimensions(-SVG_LETTER_T_WIDTH/2, SVG_Y_ORIGIN, 10), "test"},
+                                      {-LETTER_T_WIDTH/2, -ATOM_FONT_HEIGHT/2, TEST_LABEL_WIDTH, ATOM_FONT_HEIGHT});
   }
 
   void testSimpleAtomLabelWithSubscript() {
-    assertLabelAndBoundingBox("test3", {"-1.9375, 4.5, Times, 10", "17.4531, 8.5, Times, 7", "test", "3"}, {-1.9375, -8.5, 24.390625, 17});
+    assertLabelAndBoundingBox("test3", {svgDimensions(-SVG_LETTER_T_WIDTH/2, SVG_Y_ORIGIN, 10),
+                                        svgDimensions(SVG_TEST_WIDTH, SVG_Y_SUBSCRIPT, 7), "test", "3"},
+                                       {-LETTER_T_WIDTH/2, -ATOM_FONT_HEIGHT/2, TEST3_LABEL_WIDTH, ATOM_FONT_HEIGHT});
   }
 
   void testSimpleAtomLabelWithSubscriptMiddle() {
-    assertLabelAndBoundingBox("te3st", {"-1.9375, 4.5, Times, 10", "8.14063, 8.5, Times, 7", "13.1406, 4.5, Times, 10", "te", "3", "st"}, {-1.9375, -8.5, 24.390625, 17});
+    assertLabelAndBoundingBox("te3st", {svgDimensions(-SVG_LETTER_T_WIDTH/2, SVG_Y_ORIGIN, 10),
+                                        svgDimensions(SVG_TE_WIDTH, SVG_Y_SUBSCRIPT, 7),
+                                        svgDimensions(SVG_TE3_WIDTH, SVG_Y_ORIGIN, 10), "te", "3", "st"},
+                                       {-LETTER_T_WIDTH/2, -ATOM_FONT_HEIGHT/2, TEST3_LABEL_WIDTH, ATOM_FONT_HEIGHT});
   }
 
   void testSimpleAtomLabelWithSubscriptBeginning() {
-    assertLabelAndBoundingBox("3test", {"-3.5, 8.5, Times, 7", "1.5, 4.5, Times, 10", "3", "test"}, {-3.5, -8.5, 24.390625, 17});
-  }
-
-  void testSimpleAtomLabelWithSubscriptMultiple() {
-    assertLabelAndBoundingBox("te3st", {"-1.9375, 4.5, Times, 10", "8.14063, 8.5, Times, 7", "13.1406, 4.5, Times, 10", "te", "3", "st"}, {-1.9375, -8.5, 24.390625, 17});
+    assertLabelAndBoundingBox("3test", {svgDimensions(SVG_X_LEADING_SUB, SVG_Y_SUBSCRIPT, 7),
+                                        svgDimensions(SVG_X_AFTER_SUB, SVG_Y_ORIGIN, 10), "3", "test"},
+                                       {LETTER_T_WITH_3_WIDTH, -SVG_Y_SUBSCRIPT, TEST3_LABEL_WIDTH, ATOM_FONT_HEIGHT});
   }
 
   void testAtomWithTrailingHLeft() {
-    assertLabelAndBoundingBox("ABH", {"-24.4844, 4.5, Times, 10", "ABH"}, {-24.4921875, -8.5, 29.546875, 17}, Alignment::Left);
+    assertLabelAndBoundingBox("ABH", {svgDimensions(-AB_WIDTH - H_WIDTH/2, SVG_Y_ORIGIN, 10), "ABH"},
+                                     {-AB_WIDTH - H_WIDTH/2, -SVG_Y_SUBSCRIPT, AB_WIDTH + H_WIDTH, ATOM_FONT_HEIGHT}, Alignment::Left);
   }
 
   void testAtomWithTrailingHRight() {
-    assertLabelAndBoundingBox("ABH", {"-5.04688, 4.5, Times, 10", "ABH"}, {-5.0546875, -8.5, 29.546875, 17}, Alignment::Right);
+    assertLabelAndBoundingBox("ABH", {svgDimensions(-H_WIDTH/2, SVG_Y_ORIGIN, 10), "ABH"},
+                                     {-H_WIDTH/2, -SVG_Y_SUBSCRIPT, AB_WIDTH + H_WIDTH, ATOM_FONT_HEIGHT}, Alignment::Right);
   }
 
   void testAtomWithTrailingHDown() {
-    assertLabelAndBoundingBox("ABH", {"-9.71875, 4.5, Times, 10", "-9.71875, 17.5, Times, 10", "AB", "H"}, {-9.71875, -8.5, 19.4375, 30}, Alignment::Down);
+    assertLabelAndBoundingBox("ABH", {svgDimensions(-SVG_AB_WIDTH/2, SVG_Y_ORIGIN, 10),
+                                      svgDimensions(-SVG_AB_WIDTH/2, SVG_Y_SECOND_LINE, 10), "AB", "H"},
+                                     {-AB_WIDTH/2, -SVG_Y_SUBSCRIPT, AB_WIDTH, TWO_LINE_HEIGHT}, Alignment::Down);
   }
 
   void testAtomWithTrailingHUp() {
-    assertLabelAndBoundingBox("ABH", {"-9.71875, 4.5, Times, 10", "-9.71875, -8.5, Times, 10", "AB", "H"}, {-9.71875, -21.5, 19.4375, 30}, Alignment::Up);
+    assertLabelAndBoundingBox("ABH", {svgDimensions(-SVG_AB_WIDTH/2, SVG_Y_ORIGIN, 10),
+                                      svgDimensions(-SVG_AB_WIDTH/2, -SVG_Y_SUBSCRIPT, 10), "AB", "H"},
+                                     {-AB_WIDTH/2, -ATOM_FONT_METRICS.height()/2 - ATOM_FONT_METRICS.ascent(), AB_WIDTH, TWO_LINE_HEIGHT}, Alignment::Up);
   }
 
   void testAtomWithLeadingHLeft() {
-    assertLabelAndBoundingBox("HAB", {"-24.875, 4.5, Times, 10", "HAB"}, {-24.8828125, -8.5, 29.546875, 17}, Alignment::Left);
+    assertLabelAndBoundingBox("HAB", {svgDimensions(-24.8438, SVG_Y_ORIGIN, 10), "HAB"},
+                                     {-24.8515625, -SVG_Y_SUBSCRIPT, AB_WIDTH + H_WIDTH, ATOM_FONT_HEIGHT}, Alignment::Left);
   }
 
   void testAtomWithLeadingHRight() {
-    assertLabelAndBoundingBox("HAB", {"-5.04688, 4.5, Times, 10", "HAB"}, {-5.0546875, -8.5, 29.546875, 17}, Alignment::Right);
+    assertLabelAndBoundingBox("HAB", {svgDimensions(-H_WIDTH/2, SVG_Y_ORIGIN, 10), "HAB"},
+                                     {-H_WIDTH/2, -SVG_Y_SUBSCRIPT, AB_WIDTH + H_WIDTH, ATOM_FONT_HEIGHT}, Alignment::Right);
   }
 
   void testAtomWithLeadingHDown() {
-    assertLabelAndBoundingBox("HAB", {"0, 4.5, Times, 10", "HAB"}, {0, -8.5, 29.546875, 17}, Alignment::Down);
+    assertLabelAndBoundingBox("HAB", {svgDimensions(0, SVG_Y_ORIGIN, 10), "HAB"},
+                                     {0, -SVG_Y_SUBSCRIPT, AB_WIDTH + H_WIDTH, ATOM_FONT_HEIGHT}, Alignment::Down);
   }
 
   void testAtomWithLeadingHUp() {
-    assertLabelAndBoundingBox("HAB", {"0, 4.5, Times, 10", "HAB"}, {0, -8.5, 29.546875, 17}, Alignment::Up);
+    assertLabelAndBoundingBox("HAB", {svgDimensions(0, SVG_Y_ORIGIN, 10), "HAB"},
+                                     {0, -SVG_Y_SUBSCRIPT, AB_WIDTH + H_WIDTH, ATOM_FONT_HEIGHT}, Alignment::Up);
   }
 
   void testHAtomOnlyLeft() {
-    assertLabelAndBoundingBox("H", {"-5.04688, 4.5, Times, 10", "H"}, {-5.0546875, -8.5, 10.109375, 17}, Alignment::Left);
+    assertLabelAndBoundingBox("H", {svgDimensions(-H_WIDTH/2, SVG_Y_ORIGIN, 10), "H"},
+                                   {-H_WIDTH/2, -SVG_Y_SUBSCRIPT, H_WIDTH, ATOM_FONT_HEIGHT}, Alignment::Left);
   }
 
   void testHAtomOnlyRight() {
-    assertLabelAndBoundingBox("H", {"-5.04688, 4.5, Times, 10", "H"}, {-5.0546875, -8.5, 10.109375, 17}, Alignment::Right);
+    assertLabelAndBoundingBox("H", {svgDimensions(-H_WIDTH/2, SVG_Y_ORIGIN, 10), "H"},
+                                   {-H_WIDTH/2, -SVG_Y_SUBSCRIPT, H_WIDTH, ATOM_FONT_HEIGHT}, Alignment::Right);
   }
 
   void testHAtomOnlyUp() {
-    assertLabelAndBoundingBox("H", {"-5.04688, 4.5, Times, 10", "H"}, {-5.0546875, -8.5, 10.109375, 17}, Alignment::Up);
+    assertLabelAndBoundingBox("H", {svgDimensions(-H_WIDTH/2, SVG_Y_ORIGIN, 10), "H"},
+                                   {-H_WIDTH/2, -SVG_Y_SUBSCRIPT, H_WIDTH, ATOM_FONT_HEIGHT}, Alignment::Up);
   }
 
   void testHAtomOnlyDown() {
-    assertLabelAndBoundingBox("H", {"-5.04688, 4.5, Times, 10", "H"}, {-5.0546875, -8.5, 10.109375, 17}, Alignment::Down);
+    assertLabelAndBoundingBox("H", {svgDimensions(-H_WIDTH/2, SVG_Y_ORIGIN, 10), "H"},
+                                   {-H_WIDTH/2, -SVG_Y_SUBSCRIPT, H_WIDTH, ATOM_FONT_HEIGHT}, Alignment::Down);
   }
 
   void testH2OnlyLeft() {
-    assertLabelAndBoundingBox("H2", {"-11.6094, 4.5, Times, 10", "-1.5, 8.5, Times, 7", "H", "2"}, {-11.609375, -8.5, 15.109375, 17}, Alignment::Left);
+    assertLabelAndBoundingBox("H2", {svgDimensions(-11.5938, SVG_Y_ORIGIN, 10),
+                                     svgDimensions(-1.5, SVG_Y_SUBSCRIPT, 7), "H", "2"},
+                                    {-11.6015625, -SVG_Y_SUBSCRIPT, H2_WIDTH, ATOM_FONT_HEIGHT}, Alignment::Left);
   }
 
   void testH2OnlyRight() {
-    assertLabelAndBoundingBox("H2", {"-5.04688, 4.5, Times, 10", "5.04688, 8.5, Times, 7", "H", "2"}, {-5.0546875, -8.5, 15.109375, 17}, Alignment::Right);
+    assertLabelAndBoundingBox("H2", {svgDimensions(-H_WIDTH/2, SVG_Y_ORIGIN, 10),
+                                     svgDimensions(5.04688, SVG_Y_SUBSCRIPT, 7), "H", "2"},
+                                    {-H_WIDTH/2, -SVG_Y_SUBSCRIPT, H2_WIDTH, ATOM_FONT_HEIGHT}, Alignment::Right);
   }
 
   void testH2OnlyUp() {
-    assertLabelAndBoundingBox("H2", {"-7.54688, 4.5, Times, 10", "2.54688, 8.5, Times, 7", "H", "2"}, {-7.5546875, -8.5, 15.109375, 17}, Alignment::Up);
+    assertLabelAndBoundingBox("H2", {svgDimensions(-7.54688, SVG_Y_ORIGIN, 10),
+                                     svgDimensions(2.54688, SVG_Y_SUBSCRIPT, 7), "H", "2"},
+                                    {-H2_WIDTH/2, -SVG_Y_SUBSCRIPT, H2_WIDTH, ATOM_FONT_HEIGHT}, Alignment::Up);
   }
 
   void testH2OnlyDown() {
-    assertLabelAndBoundingBox("H2", {"-7.54688, 4.5, Times, 10", "2.54688, 8.5, Times, 7", "H", "2"}, {-7.5546875, -8.5, 15.109375, 17}, Alignment::Down);
+    assertLabelAndBoundingBox("H2", {svgDimensions(-7.54688, SVG_Y_ORIGIN, 10),
+                                     svgDimensions(2.54688, SVG_Y_SUBSCRIPT, 7), "H", "2"},
+                                    {-H2_WIDTH/2, -SVG_Y_SUBSCRIPT, H2_WIDTH, ATOM_FONT_HEIGHT}, Alignment::Down);
   }
 
   void testStackedHHDown() { // This is actually buggy TOOD test visually!
-    assertLabelAndBoundingBox("CHH", {"-4.65625, 4.5, Times, 10", "-4.65625, 17.5, Times, 10", "-4.65625, 30.5, Times, 10", "C", "H", "H"}, {-4.6640625, -8.5, 10.109375, 43}, Alignment::Down);
+    assertLabelAndBoundingBox("CHH", {svgDimensions(-4.65625, SVG_Y_ORIGIN, 10),
+                                      svgDimensions(-4.65625, SVG_Y_SECOND_LINE, 10),
+                                      svgDimensions(-4.65625, SVG_Y_THIRD_LINE, 10), "C", "H", "H"},
+                                     {-4.6640625, -SVG_Y_SUBSCRIPT, H_WIDTH, THREE_LINE_HEIGHT}, Alignment::Down);
   }
 
   void testStackedHHUp() { // This is actually buggy TOOD test visually!
-    assertLabelAndBoundingBox("CHH", {"-4.65625, 4.5, Times, 10", "-4.65625, -8.5, Times, 10", "-4.65625, -21.5, Times, 10", "C", "H", "H"}, {-4.6640625, -34.5, 10.109375, 43}, Alignment::Up);
+    assertLabelAndBoundingBox("CHH", {svgDimensions(-4.65625, SVG_Y_ORIGIN, 10),
+                                      svgDimensions(-4.65625, -SVG_Y_SUBSCRIPT, 10),
+                                      svgDimensions(-4.65625, -SVG_Y_SUBSCRIPT - ATOM_FONT_METRICS.ascent(), 10), "C", "H", "H"},
+                                     {-4.6640625, SVG_Y_ORIGIN - 3 * ATOM_FONT_METRICS.ascent(), H_WIDTH, THREE_LINE_HEIGHT}, Alignment::Up);
   }
 
   void testStackedHHLeft() { // This is actually buggy (bounding rect) TOOD test visually!
-    assertLabelAndBoundingBox("CHH", {"-24.4844, 4.5, Times, 10", "CHH"}, {-24.4921875, -8.5, 29.546875, 17}, Alignment::Left);
+    assertLabelAndBoundingBox("CHH", {svgDimensions(-AB_WIDTH - H_WIDTH/2, SVG_Y_ORIGIN, 10), "CHH"},
+                                     {-AB_WIDTH - H_WIDTH/2, -SVG_Y_SUBSCRIPT, AB_WIDTH + H_WIDTH, ATOM_FONT_HEIGHT}, Alignment::Left);
   }
 
   void testStackedHHRight() {  // This is actually buggy (bounding rect) TOOD test visually!
-    assertLabelAndBoundingBox("CHH", {"-4.65625, 4.5, Times, 10", "CHH"}, {-4.6640625, -8.5, 29.546875, 17}, Alignment::Right);
+    assertLabelAndBoundingBox("CHH", {svgDimensions(-4.65625, SVG_Y_ORIGIN, 10), "CHH"},
+                                     {-4.6640625, -SVG_Y_SUBSCRIPT, AB_WIDTH + H_WIDTH, ATOM_FONT_HEIGHT}, Alignment::Right);
   }
 };
