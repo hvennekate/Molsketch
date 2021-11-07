61351b0763d549a7ea76152c73094d6007040474
Hendrik Vennekate (@blackMamba) <HVennekate@gmx.de>
2021-11-07 22:18:51 +0100

    Refactor generation of atom label

--- libmolsketch/painting/textfield.cpp
+++ libmolsketch/painting/textfield.cpp
@@ -7,6 +7,9 @@
 
 namespace Molsketch {
 
+  const QRegularExpression NUMBER{"([0-9]+)"}; // TODO constants
+  const QRegularExpression NUMBER_OR_NON_NUMBER("([0-9]+|[^0-9]+)");
+
   // TODO align horizontally
   QRectF TextField::addRectFBefore(const QRectF &base, QRectF toAdd) const {
     if (!toAdd.isValid()) return base;
@@ -36,7 +39,8 @@ namespace Molsketch {
   }
 
   QPointF TextField::afterItemPostShift(const Paintable *item) const {
-    return QPointF(0, 0); //item->boundingRect().bottom());
+    Q_UNUSED(item)
+    return QPointF();
   }
 
   TextField::TextField(const TextLine *centerLine)
@@ -50,24 +54,36 @@ namespace Molsketch {
     addAfter(newLine);
   }
 
-  TextLine *hLine(int hAtomCount, const QFont &font) {
+  QString generateChargeString(int charge) {
+    if (!charge) return "";
+
+    return ((qAbs(charge) != 1) ? QString::number(qAbs(charge)) : QString())
+        + ((charge > 0) ? QChar('+') : QChar(0x2212));
+  }
+
+  TextLine *hLine(int hAtomCount, const QFont &font, const QString &chargeString = QString()) {
     auto line = new TextLine(new RegularTextBox("H", font));
-    if (hAtomCount > 1) line->addBoxRight(new StackedTextBox("", QString::number(hAtomCount), font));
+    if (hAtomCount > 1 || !chargeString.isEmpty())
+      line->addBoxRight(new StackedTextBox(chargeString, QString::number(hAtomCount), font));
     return line;
   }
 
   TextField *TextField::generateLabelForAtom(const QString &lbl, const QFont &font, Alignment alignment, quint8 hAtomCount, int charge) {
-    QRegularExpression number{"([0-9]+)"};
-    QRegularExpression numberOrNonNumber("([0-9]+|[^0-9]+)");
+    QString chargeLabel = generateChargeString(charge);
+    bool placeChargeOnHydrogens = (hAtomCount && alignment == Alignment::Right);
 
     QVector<TextBox *> boxes;
-    auto boxTexts = numberOrNonNumber.globalMatch(lbl);
+    auto boxTexts = NUMBER_OR_NON_NUMBER.globalMatch(lbl);
     while (boxTexts.hasNext()) {
       auto boxText = boxTexts.next().captured(1);
-      if (number.match(boxText).hasMatch())
-        boxes << new StackedTextBox("", boxText, font);
-      else
+      if (NUMBER.match(boxText).hasMatch()) {
+        boxes << new StackedTextBox(!boxTexts.hasNext() && !placeChargeOnHydrogens ? chargeLabel : "",
+                                    boxText, font);
+      } else {
         boxes << new RegularTextBox(boxText, font);
+        if (!boxTexts.hasNext() && !placeChargeOnHydrogens && !chargeLabel.isEmpty())
+          boxes << new StackedTextBox(chargeLabel, "", font);
+      }
     }
 
     if (boxes.empty()) return new TextField(nullptr); // TODO default constructor
@@ -76,21 +92,18 @@ namespace Molsketch {
     if (centerIterator == boxes.cend()) centerIterator = boxes.cbegin();
 
     auto line = new TextLine(*centerIterator);
-    StackedTextBox *lastBox = nullptr;
     if (centerIterator != boxes.cbegin())
       for (auto leftBoxIterator = centerIterator - 1; leftBoxIterator != boxes.cbegin(); --leftBoxIterator)
         line->addBoxLeft(*leftBoxIterator);
     for (auto rightBoxIterator = centerIterator + 1; rightBoxIterator != boxes.cend(); ++rightBoxIterator) {
-      if (boxes.cend() - rightBoxIterator != 1 || !(lastBox = dynamic_cast<StackedTextBox*>(*rightBoxIterator)))
         line->addBoxRight(*rightBoxIterator);
     }
 
     auto field = new TextField(line);
 
-    auto topLine = line;
-    if (hAtomCount >= 1) {
+    if (hAtomCount) {
       switch(alignment) {
-        case Alignment::Up: field->addLineAbove(topLine = hLine(hAtomCount, font)); break;
+        case Alignment::Up: field->addLineAbove(hLine(hAtomCount, font)); break;
         case Alignment::Down: field->addLineBelow(hLine(hAtomCount, font)); break;
         case Alignment::Left: {
             if (hAtomCount > 1) line->addBoxLeft(new StackedTextBox("", QString::number(hAtomCount), font));
@@ -98,31 +111,13 @@ namespace Molsketch {
             break;
           }
         case Alignment::Right: {
-            if (lastBox) {
-              line->addBoxRight(lastBox);
-              lastBox = nullptr;
-            }
             line->addBoxRight(new RegularTextBox("H", font));
-            if (hAtomCount > 1) {
-              lastBox = new StackedTextBox("", QString::number(hAtomCount), font);
-            }
+            line->addBoxRight(new StackedTextBox(chargeLabel,
+                                                 hAtomCount > 1 ? QString::number(hAtomCount) : "", font));
             break;
           }
       }
     }
-
-    if (!charge) {
-      if (lastBox) line->addBoxRight(lastBox);
-      return field;
-    }
-
-    QString chargeLabel;
-    if (qAbs(charge) != 1) chargeLabel += QString::number(qAbs(charge));
-    chargeLabel += (charge > 0) ? QChar('+') : QChar(0x2212);
-    auto boxToAddWithChargeString = lastBox ? new StackedTextBox(chargeLabel, lastBox->getBottomText(), font)
-                                            : new StackedTextBox(chargeLabel, "", font);
-    delete lastBox;
-    line->addBoxRight(boxToAddWithChargeString);
 
     return field;
   }
