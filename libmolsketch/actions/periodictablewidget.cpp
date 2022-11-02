ef9b7675bda23e4efc35e772c8e621692f6f2cc6
Hendrik Vennekate (@blackMamba) <HVennekate@gmx.de>
2022-11-02 11:53:30 +0100

    Update to QT 6 part 2
    * use QRegularExpression instead of QRegExp
    * adapt parsing of regular expression for frame (QRegularExpression returns only until the last non-empty match group)
    * use nullptr instead of "0"

--- libmolsketch/actions/periodictablewidget.cpp
+++ libmolsketch/actions/periodictablewidget.cpp
@@ -22,6 +22,7 @@
 #include <QButtonGroup>
 #include <QPainter>
 #include <QDebug>
+#include <QRegularExpression>
 
 namespace Molsketch {
 
@@ -53,7 +54,7 @@ namespace Molsketch {
       delete button ;
     }
 
-    QStringList periodicTable(buttonDescription.split(QRegExp("(?=[A-Z \\n])")).mid(1)) ;
+    QStringList periodicTable(buttonDescription.split(QRegularExpression("(?=[A-Z \\n])")).mid(1)) ; // TODO extract
     int periodCounter = 0,
         groupCounter  = 0;
     foreach (const QString& element, periodicTable)
